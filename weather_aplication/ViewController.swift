369cba21f558d01203e9a158a76c0d018f98edb3
Adam Głąbicki <aglabicki@airnauts.com>
2022-07-29 16:46:18 +0200

    add logo

--- weather_aplication/ViewController.swift
+++ weather_aplication/ViewController.swift
@@ -1,25 +1,37 @@
-//
-//  ViewController.swift
-//  weather_aplication
-//
-//  Created by Adam Głąbicki Airnauts on 29/07/2022.
-//
 import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let kLogoWidth: CGFloat = 250
+    private let kLogoHeight: CGFloat = 250
+    private let kLeftMargin: CGFloat = 10
+    private let kRightMargin: CGFloat = 10
+    private let kTopMargin: CGFloat = 20
+    private let aplicationImage = UIImage(named: "weather_symbol.png")
+    private let logoImageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupView()
-        
+        setupConstraints()
     }
     func setupView(){
         view.backgroundColor = .blue
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = aplicationImage
+        view.addSubview(logoImageView)
     }
     
     func setupConstraints(){
+        
+        logoImageView.snp.makeConstraints{make in
+            make.width.equalTo(kLogoWidth)
+            make.height.equalTo(kLogoHeight)
+            make.top.equalTo(view.snp.topMargin).offset(kTopMargin)
+            make.centerX.equalToSuperview()
+            }
         
     }
     
