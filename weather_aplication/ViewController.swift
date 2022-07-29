af8b20ef543a3211febf51a524fd70dba7617d9e
Adam Głąbicki <aglabicki@airnauts.com>
2022-07-29 16:09:51 +0200

    first commit

---
+++ weather_aplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather_aplication
+//
+//  Created by Adam Głąbicki Airnauts on 29/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
