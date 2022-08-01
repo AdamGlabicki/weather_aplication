288acc3aee0a61005d5ad9581099ab650a986fec
Adam Głąbicki <aglabicki@airnauts.com>
2022-08-01 12:29:50 +0200

    added UITableView

--- weather_aplication/MainViewController.swift
+++ weather_aplication/MainViewController.swift
@@ -2,8 +2,48 @@ import SnapKit
 import UIKit
 
 class MainViewControler: UIViewController {
+    private let kSideMargin: CGFloat = 10
+    private let kTopMargin: CGFloat = 20
+    private let kBottomMargin: CGFloat = 15
+    private let cityLabel = UILabel()
+    private let weatherTableView = UITableView()
+    private let cityString: String = "Warszawa"
+    private let hoursStringArray: [String] = ["8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00","19:00","20:00"]
+    private let cellReuseIdentifier = "cell"
+    
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupConstraints()
+        
+        
+    }
+    
+    func setupView() {
         view.backgroundColor = .lightGray
+        
+        cityLabel.textAlignment = .center
+        cityLabel.text = cityString
+        view.addSubview(cityLabel)
+        
+        view.addSubview(weatherTableView)
+    }
+    
+    
+    func setupConstraints() {
+        
+        cityLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.topMargin)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+        
+        weatherTableView.snp.makeConstraints { make in
+            make.top.equalTo(cityLabel.snp.bottom).offset(kTopMargin)
+            make.leftMargin.equalToSuperview()
+            make.rightMargin.equalToSuperview()
+            make.bottomMargin.equalToSuperview()
+        }
     }
 }
