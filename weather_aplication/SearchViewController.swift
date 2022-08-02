b614ff3178a6abe9308f6c5f44fc16780e56dc8c
Adam Głąbicki <aglabicki@airnauts.com>
2022-08-02 13:23:26 +0200

    added search view controller

---
+++ weather_aplication/SearchViewController.swift
@@ -0,0 +1,10 @@
+import SnapKit
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
