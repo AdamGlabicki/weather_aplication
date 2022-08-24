4c2bf4f1060f847a23d1d9942c30a7f064a1de9f
Adam Głąbicki <aglabicki@airnauts.com>
2022-08-24 10:42:03 +0200

    put switch in enum

--- weatherAplication/Model/Client/APIClient.swift
+++ weatherAplication/Model/Client/APIClient.swift
@@ -117,4 +117,40 @@ enum WeatherCodes: Int {
     case smallStorm = 95
     case storm = 96
     case heavyStorm = 99
+
+    var image: UIImage? {
+        let cloudAndSunConfig = UIImage.SymbolConfiguration(paletteColors: [UIColor.cloudColor(), UIColor.darkYellowColor()])
+        let precipitationConfig = UIImage.SymbolConfiguration(paletteColors: [UIColor.cloudColor(), .blue])
+        let boltConfig = UIImage.SymbolConfiguration(paletteColors: [UIColor.cloudColor(), UIColor.darkYellowColor()])
+
+        let cloudAndSunImage = UIImage(systemName: "cloud.sun.fill", withConfiguration: cloudAndSunConfig)
+        let snowImage = UIImage(systemName: "cloud.snow.fill", withConfiguration: precipitationConfig)
+        let rainImage = UIImage(systemName: "cloud.rain.fill", withConfiguration: precipitationConfig)
+        let heavyRainImage = UIImage(systemName: "cloud.heavyrain.fill", withConfiguration: precipitationConfig)
+        let drizzleImage = UIImage(systemName: "cloud.drizzle.fill", withConfiguration: precipitationConfig)
+        let boltImage = UIImage(systemName: "cloud.bolt.fill", withConfiguration: boltConfig)
+        let sunImage = UIImage(systemName: "sun.max.fill")
+        let cloudImage = UIImage(systemName: "cloud.fill")
+
+        switch self {
+        case .sun:
+            return sunImage?.withTintColor(UIColor.darkYellowColor(), renderingMode: .alwaysOriginal)
+        case .cloudAndSun:
+            return cloudAndSunImage
+        case .bigCloudAndSun:
+            return cloudAndSunImage
+        case .cloud:
+            return cloudImage?.withTintColor(UIColor.cloudColor(), renderingMode: .alwaysOriginal)
+        case .smallDrizzle, .drizzle, .bigDrizzle, .freezingDrizzle, .freezingBigDrizzle:
+            return drizzleImage
+        case .smallRain, .rain, .freezingRain:
+            return rainImage
+        case .heavyRain, .bigHeavyRain, .freezingSmallHeavyRain, .freezingHeavyRain, .freezingBigHeavyRain:
+            return heavyRainImage
+        case .smallSnow, .snow, .bigSnow, .heavySnow, .freezingSmallSnow, .freezingSnow:
+            return snowImage
+        case .smallStorm, .storm, .heavyStorm:
+            return boltImage
+        }
+    }
 }
