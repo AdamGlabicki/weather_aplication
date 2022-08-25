a1a12d9f8347f43821d61a95d80746e08cc43ccd
Adam Głąbicki <aglabicki@airnauts.com>
2022-08-25 13:03:44 +0200

    added delay to function

--- weatherAplication/Modules/Search/ViewModel/SearchViewModel.swift
+++ weatherAplication/Modules/Search/ViewModel/SearchViewModel.swift
@@ -6,7 +6,9 @@ class SearchViewModel {
     var delegate: SearchViewModelDelegate?
     let apiClient = APIClient.sharedInstance
 
+    var APIrequestFlag: Bool = false
     var cityInfoArray: [CityInfo] = []
+    let delay = 0.5
 
     func cityChosen(cityInfo: CityInfo) {
         delegate?.cityChosen(cityInfo: cityInfo)
@@ -14,16 +16,25 @@ class SearchViewModel {
 
     @objc
     func loadCityNames(sender: UITextField) {
-        guard let cityName = sender.text else { return }
-        apiClient.searchCities(searchTerm: cityName, completion: { [weak self] cityInfo -> Void in
-            self?.cityInfoArray = []
-            self?.cityInfoArray = cityInfo
-            self?.delegate?.cityNamesLaoded(cityNames: cityInfo)
-        }, failure: { weatherError in
-            let alert = UIAlertController(title: "Error", message: weatherError.localizedDescription, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default))
-            self.delegate?.cityNamesLoadingError(alert: alert)
-        })
+        if APIrequestFlag != true {
+            APIrequestFlag = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                guard let cityName = sender.text else {
+                    self.APIrequestFlag = false
+                    return }
+                self.apiClient.searchCities(searchTerm: cityName, completion: { [weak self] cityInfo -> Void in
+                    self?.cityInfoArray = []
+                    self?.cityInfoArray = cityInfo
+                    self?.APIrequestFlag = false
+                    self?.delegate?.cityNamesLaoded(cityNames: cityInfo)
+                }, failure: { weatherError in
+                    let alert = UIAlertController(title: "Error", message: weatherError.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.APIrequestFlag = false
+                    self.delegate?.cityNamesLoadingError(alert: alert)
+                })
+            }
+        }
     }
 
 }
